dd322359a440dce231f7f12c2bbe144a97be528a
noboruwd <noboruwd@icloud.com>
2015-03-30 13:30:26 +0800

    Initialize repository

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Delisa::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Delisa::Application.config.secret_key_base = 'a3a7a1bf712fb76be969ffb8b73437068d449d3053e00608ee8c8d3c310424eb68602d7e552cb35acab79478a4101dfc9f311f25b1c0fdf35cc910b0745f75e3'
